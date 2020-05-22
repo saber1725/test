c2f5ca00deeb98f040fc8f9eb254d1dd19ca9e77
saber1725 <saber1725@gmail.com>
2020-05-22 14:25:15 +0800

    新增會員資料與insert功能

---
+++ 會員資料/會員資料/Controllers/is會員資料Controller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using 會員資料.Models;
+
+namespace 會員資料.Controllers
+{
+    public class is會員資料Controller : Controller
+    {
+        public ActionResult list()
+        {
+            cis會員資料factory factory = new cis會員資料factory();
+            return View(factory.getAll());
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult New(cis會員資料 p)
+        {
+            new cis會員資料factory().insert(p);
+            return RedirectToAction("list");
+        }
+
+
+        // GET: is會員資料
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 會員資料/會員資料/Models/cis會員資料factory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace 會員資料.Models
+{
+    public class cis會員資料factory
+    {
+        public cis會員資料[] getAll()
+        {
+            cDataset dataset = new cDataset();
+            string sql = "select * from is會員資料";
+            return dataset.getbySQL(sql);
+        }
+
+        public cis會員資料 getByID(int is會員資料ID)
+        {
+            string sql = "select * from is會員資料 where is會員資料id = " + is會員資料ID.ToString();
+            cDataset myDataset = new cDataset();
+            cis會員資料[] users = myDataset.getbySQL(sql);
+            if(users.Length > 0)
+            {
+                return users[0];
+            }
+            return null;
+        }
+
+        internal void insert(cis會員資料 p)
+        {
+            cDataset data = new cDataset();
+            string sql = "insert into is會員資料 (";
+            sql += "is會員資料姓名,";
+            sql += "is會員資料Email,";
+            sql += "is會員資料密碼)";
+            sql += "values (";
+            sql += "'" + p.is會員資料姓名 + "',";
+            sql += "'" + p.is會員資料Email + "',";
+            sql += "'" + p.is會員資料密碼 + "')";
+            data.executeBySQL(sql);
+            
+        }
+    }
+    }
