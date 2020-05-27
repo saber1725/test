d22a8ddc0093afcdd16200df85bbb9ddf23c3f46
andy77122 <andy77122@hotmail.com>
2020-05-27 18:01:50 +0800

    20200522會員資料寫入方法串接

---
+++ 20200522會員資料寫入方法串接/20200522會員資料寫入方法串接/Controllers/newtestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using 會員資料.Models;
+
+namespace _20200522會員資料寫入方法串接.Controllers
+{
+    public class newtestController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(cis會員資料 p)
+        {
+            (new cis會員資料factory()).insert(p);
+            return View();
+        }
+    }
+}
---
+++ 20200522會員資料寫入方法串接/20200522會員資料寫入方法串接/Models/cis會員資料.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace 會員資料.Models
+{
+    public class cis會員資料
+    {
+        public int is會員資料ID { get; set; }
+        public String is會員資料姓名 { get; set; }
+        public String is會員資料暱稱 { get; set; }
+        public String is會員資料性別 { get; set; }
+        public String is會員資料連絡電話 { get; set; }
+        public String is會員資料Email { get; set; }
+        public String is會員資料密碼 { get; set; }
+        public String is會員資料聯絡地址 { get; set; }
+        public String is會員資料身分證照片 { get; set; }
+        public string is會員資料會員權限等級 { get; set; }
+
+    }
+}
