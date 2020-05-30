f7c76a551a769fd62747a2e8b0f9562c5a7a029d
ginabilish <leo10051@gmail.com>
2020-05-30 11:37:43 +0800

    05/30 作品整合測試 v1.0

---
+++ 作品頁整合/picUnionTest/Scripts/jsFunction/search_replace.js
@@ -0,0 +1,12 @@
+﻿/*Example below*/
+/*<span>@Html.ActionLink("搜尋", "w搜尋作品", "WorkSpace", new { keyword = "x" }, new { @Class = "link" })</span>@*set actionlink htmlattribute==>class*@*/
+
+
+/*jQuery replace keyword value by input value*/
+$(function(){
+    $(".link").click(function(){ /*Element target tag*/
+        var txtValue =$("#search").val(); /*Element where value extract*/
+        this.href =this.href.replace("x",txtValue);
+    });
+});
+
